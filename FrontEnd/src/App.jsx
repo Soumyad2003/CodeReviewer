4009d7f70cb05c3da33e4f485d83b3eda7653b02
Soumyadeep Seal <109369309+Soumyad2003@users.noreply.github.com>
2025-07-08 08:06:59 +0530

    Update App.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   }, [])
 
   async function reviewCode() {
-    const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+    const response = await axios.post('https://codereviewer-backend-0hrj.onrender.com/ai/get-review', { code });
     setReview(response.data)
   }
 
