3559d9a8dec873adf199bb4201c91856f1d4d7a0
Soumyadeep <somuseal21@gmail.com>
2025-07-08 08:28:55 +0530

    Changes in app.js

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use('/ai', aiRoutes)
 
 app.get('/',(req,res)=>{
-    res.send('Hello World')
+    res.send('This is the backend deployment of the web application...It looks blank but is full of complex code snippets from inside :)')
 })
 
 
